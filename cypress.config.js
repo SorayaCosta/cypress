a9d939fe6a025f600089893293c23d9a85d3f150
SorayaCosta <12.soraya@gmail.com>
2024-07-03 09:20:34 -0300

    Criação da variável

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false, //ignora possíveis erros de conteúdo inseguro e ou crossorigin
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
